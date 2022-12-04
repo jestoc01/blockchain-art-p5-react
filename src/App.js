33d8fa01f0ec1e7be0ad0d53eb2928f5116e8b31
John Stocking <jes@twc.com>
2022-12-04 11:31:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Day5 from './sketches/Day5';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Day5/>
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Generative Art with React-p5
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
